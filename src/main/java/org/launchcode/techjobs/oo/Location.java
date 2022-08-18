bbcecd1cc16d80530aec3981a16eb0cf6a47aae3
Sabrina Keeler <92762914+skeeler79@users.noreply.github.com>
2022-08-18 11:04:43 -0400

    finished refactoring other 3 classes

--- src/main/java/org/launchcode/techjobs/oo/Location.java
+++ src/main/java/org/launchcode/techjobs/oo/Location.java
@@ -2,33 +2,32 @@ package org.launchcode.techjobs.oo;
 
 import java.util.Objects;
 
-public class Location {
+public class Location extends JobField {
 
-    private int id;
-    private static int nextId = 1;
-    private String value;
-
-    public Location() {
-        id = nextId;
-        nextId++;
-    }
+//    private int id;
+//    private static int nextId = 1;
+//    private String value;
+//
+//    public Location() {
+//        id = nextId;
+//        nextId++;
+//    }
 
     // TODO: Add a constructor that takes a string as a parameter and assigns it to the 'value' field. The
     //  constructor should also call the empty constructor in order to initialize the 'id' field.
     public Location(String aValue) {
-        this ();
-        this.value = aValue;
+        super(aValue);
     }
 
     // Custom toString, equals, and hashCode methods:
 
-    @Override
-    public String toString() {
-        if (this.value.equals("")){
-            return "Data not available";
-        }else
-            return value;
-    }
+//    @Override
+//    public String toString() {
+//        if (this.value.equals("")){
+//            return "Data not available";
+//        }else
+//            return value;
+//    }
 
 
 
@@ -40,23 +39,23 @@ public class Location {
         return getId() == location.getId();
     }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(getId());
-    }
-
-    // Getters and Setters:
-
-    public int getId() {
-        return id;
-    }
-
-    public String getValue() {
-        return value;
-    }
-
-    public void setValue(String value) {
-        this.value = value;
-    }
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(getId());
+//    }
+//
+//    // Getters and Setters:
+//
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public String getValue() {
+//        return value;
+//    }
+//
+//    public void setValue(String value) {
+//        this.value = value;
+//    }
 
 }
--- src/main/java/org/launchcode/techjobs/oo/PositionType.java
+++ src/main/java/org/launchcode/techjobs/oo/PositionType.java
@@ -2,30 +2,29 @@ package org.launchcode.techjobs.oo;
 
 import java.util.Objects;
 
-public class PositionType {
+public class PositionType extends JobField{
 
-    private int id;
-    private static int nextId = 1;
-    private String value;
-
-    public PositionType() {
-        id = nextId;
-        nextId++;
-    }
+//    private int id;
+//    private static int nextId = 1;
+//    private String value;
+//
+//    public PositionType() {
+//        id = nextId;
+//        nextId++;
+//    }
 
     public PositionType(String value) {
-        this();
-        this.value = value;
+        super(value);
     }
 
     // TODO: Add a custom toString() method that returns the data stored in 'value'.
-    @Override
-    public String toString(){
-        if (this.value.equals("")){
-            return "Data not available";
-        }else
-            return value;
-    }
+//    @Override
+//    public String toString(){
+//        if (this.value.equals("")){
+//            return "Data not available";
+//        }else
+//            return value;
+//    }
 
     // TODO: Add custom equals and hashCode methods. Consider two PositionType objects "equal" when
     //  their id fields match.
@@ -38,24 +37,24 @@ public class PositionType {
         return getId() == that.getId();
     }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(getId());
-    }
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(getId());
+//    }
 
 
     // Getters and Setters:
 
-    public int getId() {
-        return id;
-    }
-
-    public String getValue() {
-        return value;
-    }
-
-    public void setValue(String value) {
-        this.value = value;
-    }
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public String getValue() {
+//        return value;
+//    }
+//
+//    public void setValue(String value) {
+//        this.value = value;
+//    }
 
 }
